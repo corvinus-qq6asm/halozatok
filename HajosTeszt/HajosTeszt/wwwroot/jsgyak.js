6314a265ab10b6ec1618060adc26594d8f31ea92
Dau Quang Phong <quang.dau@stud.uni-corvinus.hu>
2021-03-18 12:26:15 +0100

    kek

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,26 @@
+﻿window.onload = function () {
+    let hely = document.getElementById("ide");
+
+    var faktoriálisR = (n) => {
+        if (n === 0 || n === 1) {
+            return 1;
+        } else {
+            return n * faktoriálisR(n - 1)
+        }
+    }
+
+
+    for (var n = 0; n < 10; n++) {
+        let sor = document.createElement("div");
+        sor.classList.add(sor);
+        hely.appendChild(sor);
+
+        for (var k = 0; k < n; k++) {
+            let szam = document.createElement("div");
+            szam.classList.add("elem");
+            szam.innerText = faktoriálisR();
+            sor.appendChild(szam);
+        }
+    }
+}
+

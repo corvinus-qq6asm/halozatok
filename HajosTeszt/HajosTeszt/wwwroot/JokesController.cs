615faa0e05e971ba3eb990e9141a0e556fb72fd5
Dau Quang Phong <quang.dau@stud.uni-corvinus.hu>
2021-05-16 10:42:55 +0200

    sdf

---
+++ HajosTeszt/HajosTeszt/wwwroot/JokesController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmptyBoat.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokeController : ControllerBase
+    {
+        // GET: api/jokes
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/jokes
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/jokes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/jokes/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public Joke Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            return keresettVicc;
+        }
+
+        // POST api/jokes
+        [HttpPost]
+        public void Post([FromBody] Joke újVicc)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
+        }
+
+        // DELETE api/jokes/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
+        }
+
+        document.getElementById("addButton").addEventListener("click", () => {
+
+            //Ezt az objektumot fogjuk átküldeni
+            let data = {
+        jokeText: document.getElementById("jokeText").value
+    }
+
+
+        fetch("api/jokes",
+            {
+        method: 'POST',
+            headers:
+            {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+            }
+    ).then();
+
+    });
+
+    fetch("api/jokes",
+    {
+    method: 'POST',
+        headers:
+        {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(x => {
+        if (x.ok)
+        {
+            alert("Siker");
+
+        }
+        else
+        {
+            alert("Kudarc");
+        }
+    });	
+    }
+}
---
+++ HajosTeszt/HajosTeszt/wwwroot/QuestionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.wwwroot
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        public int m1()
+        { 
+        HajosTesztContext = new hajostesztcontext();
+            int kerdesdb = ContextBoundObject.questions.count();
+            return kerdesdb;
+        }
+
+        public ActionResult M2(int sorszam)
+        {
+            HajosTesztContext = new hajostesztcontext();
+            var kerdes
+        }
+
+    }
+}
